98cc87b2b8799d1a34c3defbef0dcd94a491c6d1
rafszul <rafszul@gmail.com>
2017-04-14 05:08:07 +0100

    layout mdl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { MaterialModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import 'hammerjs';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MdlModule } from '@angular-mdl/core';
 import { AngularFireModule } from 'angularfire2';
 import { environment } from '../environments/environment';
 
@@ -27,7 +28,8 @@ import { environment } from '../environments/environment';
     MaterialModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
-        AngularFireModule.initializeApp(environment.firebase)
+    MdlModule,
+    AngularFireModule.initializeApp(environment.firebase)
 
       ],
   providers: [],
